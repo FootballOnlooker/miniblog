59c6267ff3d0f0a5824fab47581b7fcbb4e93f76
antim <andrewtimofeev2013@gmail.com>
2022-08-03 13:21:17 +0300

    my mini blog

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Article
+
+
+def home(request):
+    posts = Article.objects.order_by('-created_at')
+    return render(request, 'blog/home.html', {'posts': posts})
+
+
+def test(request):
+    return HttpResponse('<h1>Test Page</h1>')
